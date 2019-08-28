2838f0033425a361479de95b2d29ee3f5e9cdd69
joewalker <assassin.joe@hotmail.com>
2019-08-28 14:24:27 +0700

    initial movies api service

---
+++ jaeger/movies/src/test/kotlin/com/jaeger/movies/MoviesApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.jaeger.movies
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
